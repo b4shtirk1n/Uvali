6d9a8730156d311fb6a5d58d1c17ecd296bff35e
b4shtirk1n <wowan4512@gmail.com>
2024-04-06 00:23:31 +0500

    first commit

--- admin/src/pages/App.jsx
+++ admin/src/pages/App.jsx
@@ -1,12 +1,15 @@
-import Analytics from "../Components/Analytics";
 import Slide from "../Components/Slide";
 import "../styles/App.scss";
 
-export default function App() {
+export default function App({ view }) {
+  App.propTypes = {
+    view,
+  };
+
   return (
     <section>
       <Slide />
-      <Analytics />
+      {view}
     </section>
   );
 }
